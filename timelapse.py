5ad0d97b9fcc881825b16a2e9d639b55598737c9
Tom Garside <tom31@hotmail.co.uk>
2018-08-26 11:06:22 +0200

    fixed names

--- timelapse.py
+++ timelapse.py
@@ -5,24 +5,24 @@ import time
 import gphoto2cffi as gphoto
 
 
-class timelapse:
-    camera_conected = false
+class TimeLapse:
+    camera_connected = False
 
     def __init__(self, interval, path):
         self.path = path
         self.interval = interval
         self.count = 0
         self.lcd = CharLCD(pin_rs=15, pin_rw=18, pin_e=16, pins_data=[21, 22, 23, 24], numbering_mode=GPIO.BOARD)
-        self.buttons = buttons(35, 40, 38)
-        self._create_Chars()
+        self.buttons = Buttons(35, 40, 38)
+        self._create_chars()
         try:
             print(gphoto.list_cameras())
             self.camera = gphoto.Camera()
-            self.camera_conected = True
+            self.camera_connected = True
         except:
             print("Please Connect a Camera")
 
-    def _updateDisplay(self, counter=0):
+    def _update_display(self, counter=0):
         self.lcd.cursor_pos = (0, 0)
         self.lcd.write_string("Interval:")
         self.lcd.cursor_pos = (0, 9)
@@ -32,7 +32,7 @@ class timelapse:
             self.lcd.write_string(str(counter))
         self.lcd.cursor_pos = (1, 0)
         self.lcd.write_string("Shots:" + str(self.count) + " ")
-        if self.camera_conected:
+        if self.camera_connected:
             self.lcd.cursor_pos = (1, 13)
             self.lcd.write_string("\x00\x01\x02")
 
@@ -68,8 +68,8 @@ class timelapse:
         self.lcd.create_char(1, bitmap1)
         self.lcd.create_char(2, bitmap2)
 
-    def _takePhoto(self):
-        # run in seperate thread to maintain timings
+    def _take_photo(self):
+        # run in separate thread to maintain timings
         image = self.camera.capture()
         with open("/home/pi/tlapse/" + self.path + str(self.count), "wb") as photo:
             photo.write(image)
@@ -77,58 +77,58 @@ class timelapse:
 
     def _wait_int(self, delay):
         while delay > 0:
-            self._updateDisplay(delay)
+            self._update_display(delay)
             delay -= 1
             time.sleep(1)
 
-    def startTimelapse(self):
-        self._updateDisplay()
+    def start_timelapse(self):
+        self._update_display()
         self._update_interval()
-        while (self.buttons.run_time_lapse()):
+        while self.buttons.run_time_lapse():
             print(str(self.buttons.run_time_lapse) + str(self.count))
             self.count += 1
-            self._updateDisplay(self.interval)
-            t = threading.Thread(self._takePhoto())
+            self._update_display(self.interval)
+            t = threading.Thread(self._take_photo())
             t.start()
             self._wait_int(self.interval)
             t.join()
         self.count = 0
 
     def _update_interval(self):
-        intervalState = self.buttons.readintervalstate()
-        if intervalState[0]:
+        interval_state = self.buttons.read_interval_state()
+        if interval_state[0]:
             self.interval += 1
-        if intervalState[1] and self.interval > 0:
+        if interval_state[1] and self.interval > 0:
             self.interval -= 1
 
 
-class buttons:
+class Buttons:
     intervalUp = False
     intervalDown = False
-    runTimelapse = False
+    runTimeLapse = False
 
-    def __init__(self, pinOne, pinTwo, pinThree):
+    def __init__(self, pin_one, pin_two, pin_three):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup([pinOne, pinTwo, pinThree], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup([pin_one, pin_two, pin_three], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        GPIO.add_event_detect(pinOne, GPIO.BOTH, callback=self.__switchTlapseState, bouncetime=400)
-        GPIO.add_event_detect(pinTwo, GPIO.FALLING, callback=self.__upInterval, bouncetime=400)
-        GPIO.add_event_detect(pinThree, GPIO.FALLING, callback=self.__downInterval, bouncetime=400)
+        GPIO.add_event_detect(pin_one, GPIO.BOTH, callback=self.__switch_tlapse_state, bouncetime=400)
+        GPIO.add_event_detect(pin_two, GPIO.FALLING, callback=self.__up_interval, bouncetime=400)
+        GPIO.add_event_detect(pin_three, GPIO.FALLING, callback=self.__down_interval, bouncetime=400)
 
-    def __switchTlapseState(self, channel):
-        self.runTimelapse = not self.runTimelapse
-        print(self.runTimelapse)
+    def __switch_tlapse_state(self, channel):
+        self.runTimeLapse = not self.runTimeLapse
+        print(self.runTimeLapse)
 
-    def __upInterval(self, channel):
+    def __up_interval(self, channel):
         self.intervalUp = True
         self.intervalDown = False
 
-    def __downInterval(self, channel):
+    def __down_interval(self, channel):
         self.intervalUp = False
         self.intervalDown = True
 
-    def readintervalstate(self):
+    def read_interval_state(self):
         up = self.intervalUp
         down = self.intervalDown
         self.intervalUp = False
@@ -136,13 +136,13 @@ class buttons:
         return [up, down]
 
     def run_time_lapse(self):
-        return self.runTimelapse
+        return self.runTimeLapse
 
 
-timel = timelapse(10, "Frame")
+timel = TimeLapse(10, "Frame")
 
 while (True):
-    timel.startTimelapse()
+    timel.start_timelapse()
 
 
 
