b0315b20be8709016849a7c1e0f61f53cb9df865
Tom Garside <tom31@hotmail.co.uk>
2018-10-03 03:40:25 -0400

    added python unittests

--- test_buttons.py
+++ test_buttons.py
@@ -5,8 +5,9 @@ import buttons
 class test_buttons(unittest.TestCase):
 
     def test_buttons_intitialzed_to_false(self):
-        mybuttons = buttons.buttons(35, 40, 38)
-        return not mybuttons.read_interval_state()[0]
+        with mock.patch('buttons.RPi.GPIO') as mocked_GPIO:
+            mybuttons = mocked_GPIO.buttons(35, 40, 38)
+            return not mybuttons.read_interval_state()[0]
 
 
 if __name__ == '__main__':
